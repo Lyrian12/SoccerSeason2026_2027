c54bd824550d0b7d03633968cb7fed232e57275c
lyrian12 <lyriannono@gmail.com>
2026-08-19 21:07:38 +0100

    Initial commit

---
+++ src/main/java/com/example/SoccerSeason/SoccerSeasonApplication.java
@@ -0,0 +1,13 @@
+package com.example.SoccerSeason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerSeasonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerSeasonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SoccerSeason/SoccerSeasonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SoccerSeason;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerSeasonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
